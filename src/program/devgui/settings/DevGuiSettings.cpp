22a137584ded8da34032cf80df584e1102f22430
Amethyst-szs <Amethyst-UwU@protonmail.com>
2023-04-06 11:56:11 -0700

    Remove unused setting entry

--- src/program/devgui/settings/DevGuiSettings.cpp
+++ src/program/devgui/settings/DevGuiSettings.cpp
@@ -22,7 +22,6 @@ DevGuiSettings::DevGuiSettings(DevGuiManager* parent)
     registerNewSetting(true, true, "Autosave");
     registerNewSetting(true, true, "Display HUD");
     registerNewSetting(true, true, "Play Music");
-    registerNewSetting(false, false, "Log File Loads");
 
     Logger::log("   Created %i settings\n", mSettings.size());
 }
